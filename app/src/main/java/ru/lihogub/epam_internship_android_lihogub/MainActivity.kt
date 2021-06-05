a86822f93e2909a15aa73f8816aa7fac1154f918
Oleg Lihogub <oligli@yandex.ru>
2021-06-05 03:03:19 +0300

    init

---
+++ app/src/main/java/ru/lihogub/epam_internship_android_lihogub/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.lihogub.epam_internship_android_lihogub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
